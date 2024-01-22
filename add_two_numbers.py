8eb4e1b913085f20350d1f239f0c25fd6be8d767
navodian1 <149151970+navodian1@users.noreply.github.com>
2024-01-22 09:45:18 +0530

    First program

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,1 +1,27 @@
-a
+# numbers can be added by different ways in python depends on what you prefer
+
+"""
+# using + operator is very common method
+num1 = 15        # you can take input from user using num1=float(input("enter first number"))
+num2 = 20
+addition = num1 + num2
+print(f"sum of {num1} and {num2} is {addition}")
+"""
+
+
+# _____________________________________________________________
+# with the help of the defined function
+def addition(num1, num2):
+    return num1 + num2
+
+
+first_num = int(input("enter first number:"))
+second_num = int(input("enter another number:"))
+result = addition(first_num, second_num)
+print(f"addition of your first value:{first_num} and second value:{second_num} is {result}")
+
+"""
+in above program it is considered that user will enter integer as input, 
+you can also use float type conversion during input and try exception block to enhance validation
+
+"""
